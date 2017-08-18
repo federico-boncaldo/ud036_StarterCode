e98915260a5259058d6ffea2d01b5c3c3453ae48
Federico Boncaldo <federico@cryptolab.net>
2017-08-18 22:04:24 +0100

    Improved the cleanliness of the code
    Shorter line, better indentation, more informative documenation.
    Code checked with PEP8 checker

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,21 +1,64 @@
+'''
+Here are defined all the instances of the class Movie,
+once all the movies are created the fresh_tomatoes
+module is used to create the HTML of the website
+and display it in the browser
+'''
 import media
 import fresh_tomatoes
 
-#instantiate all the movies
-life_is_beautiful = media.Movie("Life Is Beautiful", "Box Art", "https://www.gstatic.com/tv/thumb/movieposters/22005/p22005_p_v8_aa.jpg", "https://www.youtube.com/watch?v=pAYEQP8gx3w")
+# Life is beautiful movie: movie title, box art, poster image, youtube trailer
+life_is_beautiful = media.Movie(
+    "Life Is Beautiful",
+    "Box Art",
+    "https://www.gstatic.com/tv/thumb/movieposters/22005/p22005_p_v8_aa.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=pAYEQP8gx3w")
 
-a_space_odissey = media.Movie("2001 A Space Odyssey", "Box Art", "https://t0.gstatic.com/images?q=tbn:ANd9GcQdmmLZ7lXsw1WRy7c5qN3mka2e9ANSpHIG2INi53P6OVS8KyJo", "https://www.youtube.com/watch?v=UgGCScAV7qU")
+# A space odissey movie: movie title, box art, poster image, youtube trailer
+a_space_odissey = media.Movie(
+    "2001 A Space Odyssey",
+    "Box Art",
+    "https://t0.gstatic.com/images?q=tbn:ANd9GcQdmmLZ7lXsw1WRy7c5qN3mka2e9ANSpHIG2INi53P6OVS8KyJo",  # NOQA
+    "https://www.youtube.com/watch?v=UgGCScAV7qU")
 
-pulp_fiction = media.Movie("Pulp Fiction", "Box Art", "https://www.gstatic.com/tv/thumb/movieposters/15684/p15684_p_v8_ac.jpg", "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+# Pulp fiction movie: movie title, box art, poster image, youtube trailer
+pulp_fiction = media.Movie(
+    "Pulp Fiction",
+    "Box Art",
+    "https://www.gstatic.com/tv/thumb/movieposters/15684/p15684_p_v8_ac.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
 
-dr_strange_love = media.Movie("Dr. Strange Love", "Box Art", "https://t0.gstatic.com/images?q=tbn:ANd9GcRh424VS2g2AvU1K703L7gL66S60OM7s4FfBYkPTWDM3pTpyK2G", "https://www.youtube.com/watch?v=IutX5AGEYZc")
+# Dr. Strange Love movie: movie title, box art, poster image, youtube trailer
+dr_strange_love = media.Movie(
+    "Dr. Strange Love",
+    "Box Art",
+    "https://t0.gstatic.com/images?q=tbn:ANd9GcRh424VS2g2AvU1K703L7gL66S60OM7s4FfBYkPTWDM3pTpyK2G",  # NOQA
+    "https://www.youtube.com/watch?v=IutX5AGEYZc")
 
-gattaca = media.Movie("Gattaca", "Box Art", "https://t3.gstatic.com/images?q=tbn:ANd9GcTJCzakHOgglpJK1AqgI1HICJx9dFsEwZtDW5rQB69oM9cvz-Fu", "https://www.youtube.com/watch?v=BpzVFdDeWyo")
+# Gattaca movie: movie title, box art, poster image, youtube trailer
+gattaca = media.Movie(
+    "Gattaca",
+    "Box Art",
+    "https://t3.gstatic.com/images?q=tbn:ANd9GcTJCzakHOgglpJK1AqgI1HICJx9dFsEwZtDW5rQB69oM9cvz-Fu",  # NOQA
+    "https://www.youtube.com/watch?v=BpzVFdDeWyo")
 
-fear_and_loathing_in_las_vegas = media.Movie("Fear and Loathing in Las Vegas", "Box Art", "https://www.gstatic.com/tv/thumb/movieposters/20996/p20996_p_v8_ae.jpg", "https://www.youtube.com/watch?v=8m662obIvhY")
+# Fear and Loathing in Las Vegas movie:
+# movie title, box art, poster image, youtube trailer
+fear_and_loathing_in_las_vegas = media.Movie(
+    "Fear and Loathing in Las Vegas",
+    "Box Art",
+    "https://www.gstatic.com/tv/thumb/movieposters/20996/p20996_p_v8_ae.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=8m662obIvhY")
 
+# Array with all the movies present in the web page
+movies = [
+    life_is_beautiful,
+    a_space_odissey,
+    pulp_fiction,
+    dr_strange_love,
+    gattaca,
+    fear_and_loathing_in_las_vegas
+    ]
 
-movies = [life_is_beautiful, a_space_odissey, pulp_fiction, dr_strange_love, gattaca, fear_and_loathing_in_las_vegas]
-
-#display the movies page in the browser
+# Generate the HTML file with all the movies and open it the webbrowser
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,9 +1,11 @@
+'''The class Movie is defined by a title, poster image and a youtube trailer'''
+
+
 class Movie():
-    #initiliaze all the instance variables
+    '''The constructor of the movie class:
+    it defines the title,poster image and youtube URL'''
     def __init__(self, title, box_art, poster_image_url, trailer_youtube_url):
         self.title = title
         self.box_art = box_art
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-
-    
